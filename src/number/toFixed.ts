e9c0d1005c783537932a1d3eeadb122ff4bd5c44
王威威 <wangweiwei@qutoutiao.net>
2021-05-19 22:27:48 +0800

    refactor: 字符串翻转性能优化

--- src/number/toFixed.ts
+++ src/number/toFixed.ts
@@ -1,5 +1,5 @@
 const toFixed = (num: number, digits: number): number => {
-  return Math.floor((Math.pow(10, digits) * num)) / Math.pow(10, digits)
+  return Math.floor(Math.pow(10, digits) * num) / Math.pow(10, digits)
 }
 
 export { toFixed }
--- src/string/reverse.ts
+++ src/string/reverse.ts
@@ -1,5 +1,12 @@
 const reverse = (str: string): string => {
-  return str.split('').reverse().join('')
+  // return str.split('').reverse().join('')
+  let reversedStr = ''
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i]
+  }
+
+  return reversedStr
 }
 
 export { reverse }
